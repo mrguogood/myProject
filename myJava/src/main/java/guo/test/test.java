c3ce6910987285db54b454ae75726148c35e41f7
guochangshuai <473909598@qq.com>
2023-09-26 16:22:37 +0800

    Initial commit

---
+++ myJava/src/main/java/guo/test/test.java
@@ -0,0 +1,14 @@
+package guo.test;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class test {
+    @RequestMapping("/index")
+    @ResponseBody
+    public String index(){
+        return "hello world!";
+    }
+}
